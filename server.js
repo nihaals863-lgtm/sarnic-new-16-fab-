6503bf3fda1fbe449c0322173b7d3b1d2df8508f
GautamBairagi <gauatambairagi22@gmail.com>
2026-04-11 14:55:00 +0530

    sdsad

--- server.js
+++ server.js
@@ -5,13 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Enable CORS for all routes
-
-
-app.use(cors({
-  origin: 'https://project.phoenix-dezign.com',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type'],
-}));
+app.use(cors());
 app.use(express.json());
 
 // Health check endpoint
